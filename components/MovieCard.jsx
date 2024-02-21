d8398037b747272bc1229d7a7abc8c6248361825
P024024 <pghimire024@rku.ac.in>
2024-02-21 14:39:43 +0530

    card changes

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 
 const MovieCard = () => {
-  return <>
-  
-<div class="max-w-lg bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
-    <a href="#">
-        <img class="rounded-t-lg" src="/assets/img/welcome.jpeg" alt="" />
-    </a>
-    <div class="p-5 ">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 </p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="rtl:rotate-180 w-3.5 h-4.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a>
-    </div>
-</div>
-</>;
+  return (
+    <>
+      <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        <div class="py-3 sm:max-w-xl sm:mx-auto">
+          <div class="bg-white shadow-lg border-gray-100 max-h-80	 border sm:rounded-3xl p-8 flex space-x-8">
+            <div class="h-48 overflow-visible w-1/2">
+              <img
+                class="rounded-3xl shadow-lg"
+                src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg"
+                alt=""
+              />
+            </div>
+            <div class="flex flex-col w-1/2 space-y-4">
+              <div class="flex justify-between items-start">
+                <h2 class="text-3xl font-bold">Sweet Tooth: El niño ciervo</h2>
+                <div class="bg-yellow-400 font-bold rounded-xl p-2">7.2</div>
+              </div>
+              <div>
+                <div class="text-sm text-gray-400">Series</div>
+                <div class="text-lg text-gray-800">2019</div>
+              </div>
+              <p class=" text-gray-400 max-h-40 overflow-y-hidden">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <div class="flex text-2xl font-bold text-a">$83.90</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MovieCard;
