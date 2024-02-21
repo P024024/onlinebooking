be9ba4349de220f4a04455363e1333889cb8a4c6
P024024 <pghimire024@rku.ac.in>
2024-02-21 14:41:28 +0530

    new

--- app/movie/page.jsx
+++ app/movie/page.jsx
@@ -7,9 +7,6 @@ const Team = () => {
     <h2 className='text-2xl text-bold font-medium p-4'>Popular Movie</h2>
     <div className='flex grid grid-cols-1 md:grid-cols-4'>
 <MovieCard/>
-<MovieCard/>
-<MovieCard/>
-<MovieCard/>
     </div>
     </>
   )
